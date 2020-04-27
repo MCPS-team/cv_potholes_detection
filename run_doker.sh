fe53f55fc129db0b587478bc5d7692c42fe2ff6b
Matteo Barato <matteobarato95@gmail.com>
2020-04-27 12:14:34 +0200

    update readme and env with corrected model

--- run_doker.sh
+++ run_doker.sh
@@ -1,2 +1,2 @@
 . ./.env;
-sudo docker run -p ${HOST_PORT}:5000 --rm -it --env-file .env -v ${INPUT_IMGS_PATH}:/cv_potholes_detection/frames_in -v ${OUTPUT_IMGS_PATH}:/cv_potholes_detection/frames_out docker-cv-pothole-detect;
+sudo docker run -p ${HOST_PORT}:5000 --rm -it --env-file .env -v ${INPUT_IMGS_DIR}:/cv_potholes_detection/frames_in -v ${OUTPUT_IMGS_DIR}:/cv_potholes_detection/frames_out docker-cv-pothole-detect;
