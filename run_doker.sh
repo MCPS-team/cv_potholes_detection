126631643c04e821292c9ee888bd8d88cb4fab94
Matteo Barato <matteobarato95@gmail.com>
2020-04-27 14:31:58 +0200

    added env variable MODEL_DIR

--- run_doker.sh
+++ run_doker.sh
@@ -1,2 +1,7 @@
 . ./.env;
-sudo docker run -p ${HOST_PORT}:5000 --rm -it --env-file .env -v ${INPUT_IMGS_DIR}:/cv_potholes_detection/frames_in -v ${OUTPUT_IMGS_DIR}:/cv_potholes_detection/frames_out docker-cv-pothole-detect;
+sudo docker run -p ${HOST_PORT}:5000 --rm -it \
+    --env-file .env \
+    -v ${INPUT_IMGS_DIR}:/cv_potholes_detection/frames_in \
+    -v ${OUTPUT_IMGS_DIR}:/cv_potholes_detection/frames_out \
+    -v ${MODEL_DIR}:/cv_potholes_detection/models \
+    docker-cv-pothole-detect;
--- run_doker_test.sh
+++ run_doker_test.sh
@@ -1,2 +1,4 @@
 . ./.env;
-sudo docker run -p ${HOST_PORT}:5000 --rm -it --env-file .env -v /var/www/MCPS/cv_potholes_detection/:/cv_potholes_detection2 docker-cv-pothole-detect bash 
+sudo docker run -p ${HOST_PORT}:5000 --rm -it \
+    --env-file .env -v /var/www/MCPS/cv_potholes_detection/:/cv_potholes_detection2 \
+    docker-cv-pothole-detect bash 
